9a8128a02748cfe6cfb890db9233456e3f2fabb5
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2020-03-26 23:21:53 -0600

    Add Dream Team members

--- Tarea0/TareaHDFS.py
+++ Tarea0/TareaHDFS.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""
+Copyright [2020] [Durán Toledo, Albert Alejandro (oyente)]
+Copyright [2020] [González Quiroga, Víctor Gerardo (oyente)]
+Copyright [2020] [Pulido Ortega, Israel (oyente)]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 
 # In[26]:
 
